2955a00926549650f64cd3f80c731808661b9481
ecourse <smkang@codenuri.co.kr>
2023-03-20 11:21:53 +0900

    Create 3_CONTAINER1.cpp

---
+++ DAY1/3_CONTAINER1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+// 아래 코드는 싱글 링크드 리스트 입니다
+
+template<typename T> struct Node
+{
+	T     data;
+	Node* next;
+
+	Node(const T& a, Node* n) : data(a), next(n) {}
+};
+
+template<typename T> class slist
+{
+	Node<T>* head = 0;
+public:
+	void push_front(const T& a)
+	{
+		head = new Node<T>(a, head);
+	}
+};
+int main()
+{
+	slist<int> s;
+
+	s.push_front(10);
+	s.push_front(20);
+	s.push_front(30);
+	s.push_front(40);
+	s.push_front(50); // 이때 메모리 그림이 교재 31 page 입니다.
+}
