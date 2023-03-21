c5e2e425fe9536b1b5a15ce75e4543e25f87c963
ecourse <smkang@codenuri.co.kr>
2023-03-21 20:20:21 +0900

    Add files via upload

---
+++ DAY3/7_유틸리티.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "show.h"
+
+template<typename T> void foo(T a) {}
+
+int main()
+{
+	// pair : 서로다른 타입 2개를 보관하는 구조체
+
+}
+
+
---
+++ DAY3/7_유틸리티2.cpp
@@ -0,0 +1,8 @@
+#include <tuple>
+#include <iostream>
+
+int main()
+{
+	// pair : C++98. 서로 다른 타입의 객체 2개 보관
+	std::pair<int, double> p1(3, 3.4);
+}
