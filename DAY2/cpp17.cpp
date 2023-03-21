37c12e2cd7daf2bf8f94820f9a44cc92b5b5b9a8
ecourse <smkang@codenuri.co.kr>
2023-03-21 16:25:07 +0900

    Update cpp17.cpp

--- DAY2/cpp17.cpp
+++ DAY2/cpp17.cpp
@@ -2,7 +2,8 @@
 #include "show.h"
 #include <thread>
 #include <chrono>
-using namespace sstd::literals;
+#include <execution>
+using namespace std::literals;
 
 void foo(int n)
 {
@@ -14,8 +15,17 @@ void foo(int n)
 int main()
 {
 	std::list<int> s = { 1,2,3,4,5,6,7,8,9,10 };
-	std::for_each(s.begin(), s.end(), foo);
+
+//	std::for_each(s.begin(), s.end(), foo);
+
+	std::for_each( std::execution::par,  s.begin(), s.end(), foo);
 }
 
 
 
+// 구글에서 "github  mingw 11" 로 검색후 1번째 링크
+// ASSET 에서 207 m 받으세요
+
+// Mingw64 폴더를 통째로 원하는 위치에 복사 => 강사는 "D:\\mingw64"
+
+// g++ 경로 : "D:\mingw64\bin"
