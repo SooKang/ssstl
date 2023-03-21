7e6426bdf1ce8542d87d7d719327f545a787f5e1
ecourse <smkang@codenuri.co.kr>
2023-03-21 17:01:50 +0900

    Update 9_컨테이너와단위전략2.cpp

--- DAY2/9_컨테이너와단위전략2.cpp
+++ DAY2/9_컨테이너와단위전략2.cpp
@@ -1,9 +1,35 @@
 #include <vector>
 #include <iostream>
 
+template<typename T>
+class MyAlloc
+{
+public:
+	T* allocate(std::size_t sz) // unsigned int
+	{
+		void* p = malloc(sizeof(T) * sz);
+		printf("allocate : %p, %d cnt\n", p, sz);
+		return static_cast<T*>(p);
+	}
+	void deallocate(T* p, std::size_t sz)
+	{
+		printf("deallocate : %p, %d cnt\n", p, sz);
+		free(p);
+	}
+
+	// 위 2개 함수 외에도 아래 3개 더 필요 : 관례적인 코드
+	using value_type = T;
+
+	MyAlloc() {}	// 디폴트 생성자
+	template<typename U> MyAlloc(const MyAlloc<U>&) {} // generic생성자
+};
+
+
+
+
 int main()
 {
-	std::vector<int> v;
+	std::vector<int, MyAlloc<int> > v;
 
 	std::cout << "----------------" << std::endl;
 	v.resize(5);
