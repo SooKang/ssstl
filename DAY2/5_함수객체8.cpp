7e29183ff03efb6db62391c1489c70818c6115c7
ecourse <smkang@codenuri.co.kr>
2023-03-21 14:55:35 +0900

    Update 5_함수객체8.cpp

--- DAY2/5_함수객체8.cpp
+++ DAY2/5_함수객체8.cpp
@@ -1,5 +1,6 @@
 // 5_함수객체8
 #include "show.h"
+#include <functional>  // std::less, std::greater 등의 함수 객체가 이미 제공
 
 int main()
 {
@@ -9,5 +10,27 @@ int main()
 	std::sort(v.begin(), v.end());
 
 	// 비교 정책 교체 1. 일반 함수
-	std::sort(v.begin(), v.end(), cmp1);  
+//	std::sort(v.begin(), v.end(), cmp1);  
+
+
+	// 비교 정책 교체 2. C++ 표준의 함수 객체 사용 - C++98 시절 부터
+	std::less<int> f;
+	std::sort(v.begin(), v.end(), f);
+
+	std::sort(v.begin(), v.end(), std::less<int>() ); // 임시객체로 전달
+
+
+	// 비교 정책 교체 3. 람다 표현식 - C++11 부터
+	std::sort(v.begin(), v.end(), [](int a, int b) { return a < b; } );
+
+	// 위 코드를 보고 컴파일러는 아래 코드를 생성합니다.
+	class CompilerGeneratedName
+	{
+	public:
+		inline bool operator()(int a, int b) { return a < b; }
+	};
+
+	std::sort(v.begin(), v.end(), CompilerGeneratedName() );
+
+
 }
