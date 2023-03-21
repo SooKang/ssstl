80be03fc2d7be8d99a77919e8cfcd4a5d52a73de
ecourse <smkang@codenuri.co.kr>
2023-03-21 16:54:01 +0900

    Update 9_컨테이너와단위전략1.cpp

--- DAY2/9_컨테이너와단위전략1.cpp
+++ DAY2/9_컨테이너와단위전략1.cpp
@@ -1,14 +1,21 @@
 #include <vector>
 
-// resize �� ������ ������ ���ô�.
-template<typename T> 
+// resize 의 원리를 생각해 봅시다.
+template<typename T, typename Alloc = std::allocator<T> > 
 class vector
 {
+	Alloc ax; // 메모리 할당기 객체
+			  // vector 의 모든 멤버 함수는 메모리 할당이 필요하면
+			  // 이 객체(ax) 를 사용해야 합니다.
 public:
 	void resize(int sz)
 	{
-		// �޸� �Ҵ��� �ʿ��� ��� ��� �ұ�� ?
+		// 메모리 할당이 필요한 경우 어떻게 할까요 ?
 		// new ? malloc ? memory pool
+
+		T* p = ax.allocate(sz);
+
+		ax.deallocate(p, sz);
 	}
 };
 
