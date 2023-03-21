2701280621219e6112a45d198f828adbcc1128f1
ecourse <smkang@codenuri.co.kr>
2023-03-21 09:02:36 +0900

    Update 1_stream4.cpp

--- DAY2/1_stream4.cpp
+++ DAY2/1_stream4.cpp
@@ -5,16 +5,27 @@
 
 int main()
 {
-	std::ostream_iterator<char> out(std::cout, "");
+//	std::ostream_iterator<char> out(std::cout, "");
+
+	std::ofstream fout("b.txt");
+	std::ostream_iterator<char> out(fout, "");
+
 
 	std::ifstream f("1_stream4.cpp"); // 입력 파일
 
-	std::istream_iterator<char> p1(f);
-	std::istream_iterator<char> p2;		// 디폴트 생성자는 EOF
+//	std::istream_iterator<char> p1(f);
+//	std::istream_iterator<char> p2;		// 디폴트 생성자는 EOF
 
-	std::cout << *p1 << std::endl;
+	// istream_iterator    : white space 무시(space, tab, enter등)
+	// istreambuf_iterator : white space 도 입력!
+	std::istreambuf_iterator<char> p1(f);
+	std::istreambuf_iterator<char> p2;		
 
-//	std::copy(p1, p2, out);
+
+
+//	std::cout << *p1 << std::endl;
+
+	std::copy(p1, p2, out);
 }
 
 
