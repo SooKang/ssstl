634d8123870d57252a4e2651f0734f0e891fcf48
ecourse <smkang@codenuri.co.kr>
2023-03-22 11:00:56 +0900

    Update 참고2.cpp

--- DAY3/참고2.cpp
+++ DAY3/참고2.cpp
@@ -24,6 +24,12 @@ int main()
 
 	// 메모리 할당과 생성자 호출을 분리하면 유연성이 좋아 집니다.
 	Point* p2 = static_cast<Point*>( operator new(sizeof(Point) * 10));
+
+	// 이미 할당된 메모리에 10개의 객체 생성(생성자 호출)
+	for (int i = 0; i < 10; i++)
+	{
+		new (&p2[i]) Point(0, 0);
+	}
 }
 
 // malloc : 메모리 할당
